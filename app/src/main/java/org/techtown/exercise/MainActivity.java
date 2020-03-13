487c9b65ff4816942287cdb130cb2d678f78f52b
HwangGyuBin <tkrlghfh2@naver.com>
2020-03-13 11:32:43 +0900

    3/13 아이콘, 검색 아이콘, 입력창, 광고배너 구현

---
+++ app/src/main/java/org/techtown/exercise/MainActivity.java
@@ -0,0 +1,33 @@
+package org.techtown.exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    EditText editText2;
+    private AdView mAdView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+}
