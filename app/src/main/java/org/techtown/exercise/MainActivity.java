69a5f6a6892ca15b359906b1b59322de34f63906
HwangGyuBin <tkrlghfh2@naver.com>
2020-03-19 21:58:54 +0900

    3/19 api 사용 - 오류

--- app/src/main/java/org/techtown/exercise/MainActivity.java
+++ app/src/main/java/org/techtown/exercise/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -19,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
@@ -27,7 +38,17 @@ public class MainActivity extends AppCompatActivity {
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
-    }
 
+
+        //------------------------------------------------------------------
+
+        MapView mapView = new MapView(this);
+
+        RelativeLayout container= (RelativeLayout) findViewById(R.id.map_view);
+        container.addView(mapView);
+
+
+
+    }
 
 }
